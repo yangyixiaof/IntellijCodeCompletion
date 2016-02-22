c60e0f427ded69623053a27bbb8c0b3a459f4027
yyx <yangyixiaofirst@163.com>
2016-02-22 20:56:09 +0800

    small change.

--- IntelliJCodeCompletion/src/cn/yyx/contentassist/codepredict/CodeNGramAnalyzer.java
+++ IntelliJCodeCompletion/src/cn/yyx/contentassist/codepredict/CodeNGramAnalyzer.java
@@ -2,13 +2,27 @@ package cn.yyx.contentassist.codepredict;
 
 import java.util.ArrayList;
 
-import org.eclipse.jface.text.IDocument;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
 
 public class CodeNGramAnalyzer {
 
-	public static ArrayList<String> PossibleCodes(IDocument doc, int offset) {
+	public static ArrayList<String> PossibleCodes(JavaContentAssistInvocationContext javacontext, int offset) {
 		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+			ICompilationUnit cu = javacontext.getCompilationUnit();
+			IJavaElement[] childs = cu.getChildren();
+			for (IJavaElement child : childs)
+			{
+				System.out.println("ElementName:"+child.toString());
+			}
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 	
 }
--- IntelliJCodeCompletion/src/cn/yyx/contentassist/codepredict/CodePredictUtil.java
+++ IntelliJCodeCompletion/src/cn/yyx/contentassist/codepredict/CodePredictUtil.java
@@ -29,8 +29,10 @@ public class CodePredictUtil implements CodePredict {
 			IDocument doc = javacontext.getDocument();
 			int offset = javacontext.getInvocationOffset();
 			char lastchar = DocumentContentHelper.GetOffsetLastChar(doc, offset);
-			if (lastchar == ';' || lastchar == '}' || lastchar == ',' || lastchar == '{' || lastchar == '(') {
-				ArrayList<String> proposalcnt = CodeNGramAnalyzer.PossibleCodes(doc, offset);
+			if (lastchar == ';' || lastchar == '}') {
+				// detailed completion will be realized later. this is difficult in technique.
+				//  || lastchar == ',' || lastchar == '{' || lastchar == '('
+				ArrayList<String> proposalcnt = CodeNGramAnalyzer.PossibleCodes(javacontext, offset);
 				ProposalHelper.ProposalContentToFormalFormat(javacontext, proposalcnt, proposals);
 			} else {
 				proposals.add(new CompletionProposal(IntelliJavaProposalComputer.OnlyExpressionSupport,
