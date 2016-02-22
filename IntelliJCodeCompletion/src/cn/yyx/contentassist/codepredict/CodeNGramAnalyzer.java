25d9429fb9cea7c0e2ea1408419a4ca7c99de509
yyx <yangyixiaofirst@163.com>
2016-02-22 17:07:58 +0800

    small change.

---
+++ IntelliJCodeCompletion/src/cn/yyx/contentassist/codepredict/CodeNGramAnalyzer.java
@@ -0,0 +1,14 @@
+package cn.yyx.contentassist.codepredict;
+
+import java.util.ArrayList;
+
+import org.eclipse.jface.text.IDocument;
+
+public class CodeNGramAnalyzer {
+
+	public static ArrayList<String> PossibleCodes(IDocument doc, int offset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
--- IntelliJCodeCompletion/src/cn/yyx/contentassist/codepredict/CodePredictUtil.java
+++ IntelliJCodeCompletion/src/cn/yyx/contentassist/codepredict/CodePredictUtil.java
@@ -4,15 +4,48 @@ import java.util.ArrayList;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.ui.text.java.ContentAssistInvocationContext;
+import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
-public class CodePredictUtil implements CodePredict{
+import cn.yyx.contentassist.codecompletion.IntelliJavaProposalComputer;
+import commonutils.DocumentContentHelper;
+import commonutils.ProposalHelper;
+
+public class CodePredictUtil implements CodePredict {
 
 	@Override
 	public ArrayList<ICompletionProposal> PredictCodes(ContentAssistInvocationContext context,
 			IProgressMonitor monitor) {
-		
-		return null;
+		ArrayList<ICompletionProposal> proposals = new ArrayList<ICompletionProposal>();
+		if (!(context instanceof JavaContentAssistInvocationContext)) {
+			NotifyError(proposals, context);
+			return proposals;
+		}
+		try {
+			JavaContentAssistInvocationContext javacontext = (JavaContentAssistInvocationContext) context;
+			IDocument doc = javacontext.getDocument();
+			int offset = javacontext.getInvocationOffset();
+			char lastchar = DocumentContentHelper.GetOffsetLastChar(doc, offset);
+			if (lastchar == ';' || lastchar == '}' || lastchar == ',' || lastchar == '{' || lastchar == '(') {
+				ArrayList<String> proposalcnt = CodeNGramAnalyzer.PossibleCodes(doc, offset);
+				ProposalHelper.ProposalContentToFormalFormat(javacontext, proposalcnt, proposals);
+			} else {
+				proposals.add(new CompletionProposal(IntelliJavaProposalComputer.OnlyExpressionSupport,
+						context.getInvocationOffset(), 0, IntelliJavaProposalComputer.OnlyExpressionSupport.length()));
+			}
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+		return proposals;
+	}
+
+	private void NotifyError(ArrayList<ICompletionProposal> proposals, ContentAssistInvocationContext context) {
+		System.err.println(IntelliJavaProposalComputer.OnlyJavaSupport);
+		proposals.add(new CompletionProposal(IntelliJavaProposalComputer.OnlyJavaSupport, context.getInvocationOffset(),
+				0, IntelliJavaProposalComputer.OnlyJavaSupport.length()));
 	}
 	
 }
